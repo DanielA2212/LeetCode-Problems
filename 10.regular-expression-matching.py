ba3e3684cad1f7b8932f66a32469297439e25558
Daniel Agranovski <danielagr@my.hit.ac.il>
2025-03-12 05:42:48 +0200

    First SetUp

---
+++ 10.regular-expression-matching.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=10 lang=python
+#
+# [10] Regular Expression Matching
+#
+
+# @lc code=start
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+
+        print('hello mt')
+        
+# @lc code=end
+
---
+++ 4.median-of-two-sorted-arrays.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=4 lang=python
+#
+# [4] Median of Two Sorted Arrays
+#
+
+# @lc code=start
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        print("hello world")
+
+# @lc code=end
+
