83554b34c0ea99ef79297b67c5e9d5980a7d1aec
Daniel Agranovski <danielagr@my.hit.ac.il>
2025-03-19 04:10:09 +0200

    solved 1 problem

--- Solved/135.candy.py
+++ Solved/135.candy.py
@@ -18,19 +18,17 @@ class Solution(object):
         if n == 1:
             return 1
 
+        candies = [1] * n
 
+        for i in range(1, n):
+            if ratings[i] > ratings[i-1]:
+                candies[i] = candies[i-1] + 1
 
-        # candies = [1] * n
+        for i in range(n-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                candies[i] = max(candies[i], candies[i+1] + 1)
 
-        # for i in range(1, n):
-        #     if ratings[i] > ratings[i-1]:
-        #         candies[i] = candies[i-1] + 1
-
-        # for i in range(n-2, -1, -1):
-        #     if ratings[i] > ratings[i+1]:
-        #         candies[i] = max(candies[i], candies[i+1] + 1)
-
-        # return sum(candies)
+        return sum(candies)
         
 # @lc code=end
 
