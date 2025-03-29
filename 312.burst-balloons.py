84ddfd569a243c6c745b3a2894aca00b68b810e3
DanielA2212 <danielawork98@gmail.com>
2025-03-29 03:37:17 +0300

    Added New Problems

---
+++ 312.burst-balloons.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=312 lang=python
+#
+# [312] Burst Balloons
+#
+
+# @lc code=start
+class Solution(object):
+    def maxCoins(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+# @lc code=end
+
---
+++ 664.strange-printer.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=664 lang=python
+#
+# [664] Strange Printer
+#
+
+# @lc code=start
+class Solution(object):
+    def strangePrinter(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+# @lc code=end
+
