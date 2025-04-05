a7443a51bdd60aa8c9763815ba6c4fabb8e52840
DanielA2212 <danielawork98@gmail.com>
2025-04-05 20:02:09 +0300

    solved some

---
+++ Solved/1250.check-if-it-is-a-good-array.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode id=1250 lang=python
+#
+# [1250] Check If It Is a Good Array
+#
+
+# @lc code=start
+class Solution(object):
+    def isGoodArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        #credit to https://stackoverflow.com/a/11175154 for the gcd function
+        def gcd(a, b):
+          
+            while b:
+                a, b = b, a%b
+            return a
+        
+        #credit to https://stackoverflow.com/a/9109065 for the reduce function
+        def reduce(func, iterable, start=None):
+
+            it = iter(iterable)
+            if start is None:
+                try:
+                   start = next(it)
+                except StopIteration:
+                   raise TypeError('reduce() of empty sequence with no initial value')
+            accum_value = start
+            for x in it:
+                  accum_value = func(accum_value, x)
+            return accum_value
+        
+
+        overall_gcd = reduce(gcd, nums)
+        return overall_gcd == 1
+        
+# @lc code=end
+
---
+++ Solved/887.super-egg-drop.py
@@ -0,0 +1,46 @@
+#
+# @lc app=leetcode id=887 lang=python
+#
+# [887] Super Egg Drop
+#
+
+# @lc code=start
+class Solution(object):
+    def superEggDrop(self, k, n):
+        """
+        :type k: int
+        :type n: int
+        :rtype: int
+        """
+
+        # Credit to: https://stackoverflow.com/questions/26560726/python-binomial-coefficient
+        # Calculates the binomial coefficient C(m, k)
+        # which represents the number of ways to choose k items from m items
+
+        def binocoe(m, k):
+            
+            if k > m:
+                return 0
+            res = 1
+            for i in range(k):
+                res = res * (m - i) // (i + 1)
+            return res
+
+        # credit to: https://stackoverflow.com/a/212413 for binary search
+        
+        left, right = 1, n
+        while left < right:
+            mid = (left + right) // 2
+            total = 0
+            for i in range(1, k + 1):
+                total += binocoe(mid, i)
+                if total >= n:
+                    break
+            if total >= n:
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
+# @lc code=end
+
