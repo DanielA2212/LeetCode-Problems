dbeabf538ce1670dbcca6b57cd40f8a8b2a7b5fc
DanielA2212 <danielawork98@gmail.com>
2025-04-29 14:41:26 +0300

    added

---
+++ New/3272.find-the-count-of-good-integers.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=3272 lang=python
+#
+# [3272] Find the Count of Good Integers
+#
+
+# @lc code=start
+class Solution(object):
+    def countGoodIntegers(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        
+# @lc code=end
+
