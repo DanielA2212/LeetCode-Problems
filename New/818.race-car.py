ac7e8578ca2bb2731b58eb9092fa771512d42fc2
DanielA2212 <danielawork98@gmail.com>
2025-05-02 02:27:49 +0300

    added race car

---
+++ New/818.race-car.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=818 lang=python
+#
+# [818] Race Car
+#
+
+# @lc code=start
+class Solution(object):
+    def racecar(self, target):
+        """
+        :type target: int
+        :rtype: int
+        """
+        
+        
+# @lc code=end
+
